6c9703dd6db533bf46afabe8def6c5b077024eed
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-09-30 23:40:52 +0700

    cargame: removed bat script for compilation

--- cargame/compile_win.bat
+++
@@ -1,6 +0,0 @@
-g++ -c ./main.cpp -o ./bin/main.o
-g++ -c ./libs/player.cpp -o ./bin/player.o
-g++ -c ./libs/cursor.cpp -o ./bin/cursor.o
-g++ -c ./libs/road.cpp -o ./bin/road.o
-g++ -c ./libs/game.cpp -o ./bin/game.o
-g++ ./bin/main.o ./bin/cursor.o ./bin/game.o ./bin/road.o ./bin/player.o -o ./cargame
